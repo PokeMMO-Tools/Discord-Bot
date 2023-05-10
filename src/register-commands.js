5baa63d8245677af2cf6f29d911ce24053638be7
Cosmin <cosmintutoveanu00@gmail.com>
2023-05-10 18:30:50 +1000

    multi server support

--- src/register-commands.js
+++ src/register-commands.js
@@ -47,7 +47,7 @@ const commands = [
  */
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
+//PokeMMO Hub server
 (async () => {
   try {
     console.log("Registering slash commands");
@@ -56,6 +56,24 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
         process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Slash commands were registered correctly");
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
+//second server (presumably patrouski's)
+(async () => {
+  try {
+    console.log("Registering slash commands");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID2
       ),
       { body: commands }
     );
