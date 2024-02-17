f28b98cd5fd476e2027518f626cc1a41d6fe3e5e
KasaiKonoru <45830738+KasaiKonoru@users.noreply.github.com>
2024-02-17 13:22:51 +0100

    removed color in crashhandler

--- src/crashhandler.js
+++ src/crashhandler.js
@@ -1,23 +1,23 @@
 process.on('unhandledRejection', (reason, p) => {
-    console.log('\n\n\n\n\n=== unhandled Rejection ==='.toUpperCase().yellow.dim);
-    console.log('Reason: ', reason.stack ? String(reason.stack).gray : String(reason).gray);
-    console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase().yellow.dim);
+    console.log('\n\n\n\n\n=== unhandled Rejection ==='.toUpperCase());
+    console.log('Reason: ', reason.stack ? String(reason.stack) : String(reason));
+    console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase());
 });
 process.on("uncaughtException", (err, origin) => {
-    console.log('\n\n\n\n\n\n=== uncaught Exception ==='.toUpperCase().yellow.dim);
+    console.log('\n\n\n\n\n\n=== uncaught Exception ==='.toUpperCase());
     console.log('Exception: ', err.stack ? err.stack : err)
-    console.log('=== uncaught Exception ===\n\n\n\n\n'.toUpperCase().yellow.dim);
+    console.log('=== uncaught Exception ===\n\n\n\n\n'.toUpperCase());
 })
 process.on('uncaughtExceptionMonitor', (err, origin) => {
-    console.log('=== uncaught Exception Monitor ==='.toUpperCase().yellow.dim);
+    console.log('=== uncaught Exception Monitor ==='.toUpperCase());
 });
 process.on('beforeExit', (code) => {
-    console.log('\n\n\n\n\n=== before Exit ==='.toUpperCase().yellow.dim);
+    console.log('\n\n\n\n\n=== before Exit ==='.toUpperCase());
     console.log('Code: ', code);
-    console.log('=== before Exit ===\n\n\n\n\n'.toUpperCase().yellow.dim);
+    console.log('=== before Exit ===\n\n\n\n\n'.toUpperCase());
 });
 process.on('exit', (code) => {
-    console.log('\n\n\n\n\n=== exit ==='.toUpperCase().yellow.dim);
+    console.log('\n\n\n\n\n=== exit ==='.toUpperCase());
     console.log('Code: ', code);
-    console.log('=== exit ===\n\n\n\n\n'.toUpperCase().yellow.dim);
+    console.log('=== exit ===\n\n\n\n\n'.toUpperCase());
 });
