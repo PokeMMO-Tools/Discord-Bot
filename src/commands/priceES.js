8ee227703695a8b35c08aaac93f5e11c35e11907
Cosmin <cosmintutoveanu00@gmail.com>
2025-02-20 12:25:54 +1100

    Update priceES.js

--- src/commands/priceES.js
+++ src/commands/priceES.js
@@ -138,7 +138,7 @@ const onExecute = async (interaction) => {
 
     chart.backgroundColor = "#1b1b1b";
     const url = await chart.getShortUrl();
-    const slug = toSlug(itemData["n"]["es"])
+    const slug = toSlug(itemData["n"]["en"])
     const chartEmbed = new EmbedBuilder()
         .setColor("Random")
         .setTitle(itemData["n"]["en"])
